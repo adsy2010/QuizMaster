7d6ef2373b47f0498261e912dad1d40b04ef111a
adsy2010 <adsy2010@users.noreply.github.com>
2021-08-21 21:48:01 +0100

    Added vue components question form, quiz form and quiz question list

---
+++ resources/js/components/question/QuestionForm.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <b-form-group label="Question">
+            <b-input v-model="question"></b-input>
+        </b-form-group>
+
+        <b-row>
+            <b-col>
+                <b-form-group label="Answer 1">
+                    <b-input-group>
+                        <b-input v-model="answer1"></b-input>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
+            <b-col>
+                <b-form-group label="Answer 2">
+                    <b-input-group>
+                        <b-input-group-prepend is-text>
+                            <input v-model="answer2Enabled" type="checkbox" aria-label="Enable answer 2">
+                        </b-input-group-prepend>
+                        <b-input v-model="answer2" :disabled="!answer2Enabled"></b-input>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <b-col>
+                <b-form-group label="Answer 3">
+                    <b-input-group>
+                        <b-input-group-prepend is-text>
+                            <input v-model="answer3Enabled" type="checkbox" aria-label="Enable answer 3">
+                        </b-input-group-prepend>
+                        <b-input v-model="answer3" :disabled="!answer3Enabled"></b-input>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
+            <b-col>
+                <b-form-group label="Answer 4">
+                    <b-input-group>
+                        <b-input-group-prepend is-text>
+                            <input v-model="answer4Enabled" type="checkbox" aria-label="Enable answer 4">
+                        </b-input-group-prepend>
+                        <b-input v-model="answer4" :disabled="!answer4Enabled"></b-input>
+                    </b-input-group>
+                </b-form-group>
+            </b-col>
+        </b-row>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "QuestionForm",
+    data() {
+        return {
+            question: '',
+            answer1: '',
+            answer2: '',
+            answer3: '',
+            answer4: '',
+            answer2Enabled: false,
+            answer3Enabled: false,
+            answer4Enabled: false,
+        }
+    }
+}
+</script>
