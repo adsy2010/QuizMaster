45f5d15d9ce51fefbde8d5d6454dac98f8d09dcf
adsy2010 <adsy2010@users.noreply.github.com>
2021-08-22 00:10:30 +0100

    Added quiz form name input

--- resources/js/components/quiz/QuizForm.vue
+++ resources/js/components/quiz/QuizForm.vue
@@ -1,9 +1,20 @@
 <template>
-
+    <div>
+        <b-form-group
+            label="name"
+        >
+            <b-input v-model="name"></b-input>
+        </b-form-group>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "QuizForm"
+    name: "QuizForm",
+    data() {
+        return {
+            name: '',
+        }
+    }
 }
 </script>
