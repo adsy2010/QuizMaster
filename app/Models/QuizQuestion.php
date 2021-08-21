7424ecba38ee194e05de658329509144c2526cbf
adsy2010 <adsy2010@users.noreply.github.com>
2021-08-21 21:50:24 +0100

    Added quiz and question relationship

--- app/Models/QuizQuestion.php
+++ app/Models/QuizQuestion.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class QuizQuestion extends Model
@@ -14,4 +15,20 @@ class QuizQuestion extends Model
         'quiz_id',
         'question_id'
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
