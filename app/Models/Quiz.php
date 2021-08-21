e023574b060d44e9d51507878d86d0375c85e869
adsy2010 <adsy2010@users.noreply.github.com>
2021-08-21 21:50:10 +0100

    Added questions relationship

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -14,4 +14,9 @@ class Quiz extends Model
         'user_id',
         'name',
     ];
+
+    public function questions()
+    {
+        return $this->hasManyThrough(Question::class, QuizQuestion::class, 'quiz_id', 'id', 'id', 'question_id');
+    }
 }
