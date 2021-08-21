0545e4f6c3f65e433fbd32866827bca6c7fcefaa
adsy2010 <adsy2010@users.noreply.github.com>
2021-08-22 00:10:48 +0100

    Added quiz form and quiz question list components

--- resources/js/components/quiz/QuizCreate.vue
+++ resources/js/components/quiz/QuizCreate.vue
@@ -1,10 +1,18 @@
 <template>
 
+    <div>
+        <QuizForm></QuizForm>
+        <QuizQuestionList></QuizQuestionList>
+    </div>
+
 </template>
 
 <script>
+import QuizForm from "./QuizForm";
+import QuizQuestionList from "./QuizQuestionList";
 export default {
-    name: "QuizCreate"
+    name: "QuizCreate",
+    components: {QuizQuestionList, QuizForm}
 }
 </script>
 
